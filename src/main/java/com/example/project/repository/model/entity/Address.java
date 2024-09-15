e0afb582acedc19ab0b182d3a8a3bd9c84165e3d
Manasvi Jain <113703791+manasviJain27@users.noreply.github.com>
2024-09-15 18:17:40 -0400

    Create Address.java

---
+++ src/main/java/com/example/project/repository/model/entity/Address.java
@@ -0,0 +1,51 @@
+package com.example.project.repository.model.entity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Address {
+    @Id
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address(String street, String city, String state, String zipCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
